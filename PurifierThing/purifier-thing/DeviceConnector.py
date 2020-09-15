94a18d8eb6169c42f8c625b3218b968e415347c1
Groebehavn <pepo.tamas@gmail.com>
2020-09-15 20:50:20 +0200

    Initial commit with dependency to py-air-control

---
+++ PurifierThing/purifier-thing/DeviceConnector.py
@@ -0,0 +1,17 @@
+'''
+Created on 2020. szept. 15.
+
+@author: pepot
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
---
+++ PurifierThing/purifier-thing/PurifierThing.py
@@ -0,0 +1,8 @@
+'''
+Created on 2020. szept. 15.
+
+@author: pepot
+'''
+
+if __name__ == '__main__':
+    pass
